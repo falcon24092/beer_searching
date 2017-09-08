13aa84013ab06b0bffdccf627701471c0f743bb6
BankBankBank <b____ank@hotmail.com>
2017-09-08 18:55:18 +0700

    Update Again

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -8,9 +8,19 @@ var getBeersAPI = function getBeersAPI() {
     }).then(function (response) {
         var beers = response;
         var result = "";
+        
         for (var i = 0; i < beers.length; i++) {
-            result += "<b>Name : </b>" + beers[i].name + "<br> <b>Tagline : </b>" + beers[i].tagline + "<br> <b>PH : </b>" + beers[i].ph + "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast + "<br> <b>First Brewed : </b>" + beers[i].first_brewed + "<br> <b>Description : </b>" + beers[i].description + "<br>" + "<image src =" + beers[i].image_url + "/><hr>";
+            result += "<b>Name : </b>" + beers[i].name +
+                      "<br> <b>Tagline : </b>" + beers[i].tagline + 
+                      "<br> <b>PH : </b>" + beers[i].ph + 
+                      "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast + 
+                      "<br> <b>First Brewed : </b>" + beers[i].first_brewed + 
+                      "<br> <b>Description : </b>" + beers[i].description + 
+                      "<br>" + 
+                      "<image src =" + beers[i].image_url + " /><hr>";
         }
+        
+
         document.getElementById('result').innerHTML = result;
     });
 };
@@ -24,8 +34,17 @@ var getBeerAPIByName = function getBeerAPIByName(name) {
         var beers = response;
         var result = "";
         for (var i = 0; i < beers.length; i++) {
-            result += "<b>Name : </b>" + beers[i].name + "<br> <b>Tagline : </b>" + beers[i].tagline + "<br> <b>PH : </b>" + beers[i].ph + "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast + "<br> <b>First Brewed : </b>" + beers[i].first_brewed + "<br> <b>Description : </b>" + beers[i].description + "<br>" + "<image src =" + beers[i].image_url + "/><hr>";
+            result += "<b>Name : </b>" + beers[i].name + 
+                      "<br> <b>Tagline : </b>" + beers[i].tagline + 
+                      "<br> <b>PH : </b>" + beers[i].ph + 
+                      "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast + 
+                      "<br> <b>First Brewed : </b>" + beers[i].first_brewed + 
+                      "<br> <b>Description : </b>" + beers[i].description + 
+                      "<br>" + 
+                      "<image src =" + beers[i].image_url + " /><hr>";
         }
+        
+        
         document.getElementById('result').innerHTML = result;
     });
 };
