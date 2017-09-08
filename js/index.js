b5ec62464dfaacfd25cb4af8cdbed52b46ec06e3
BankBankBank <b____ank@hotmail.com>
2017-09-08 16:43:15 +0700

    Add Everything

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const getBeersAPI = () => fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .catch(e => console.err('Fail'))
+    .then(response => {
+        let beers = response;
+        let result = "";
+        for (let i = 0; i < beers.length; i++) {
+            result += "<b>Name : </b>" + beers[i].name +
+                "<br> <b>Tagline : </b>" + beers[i].tagline +
+                "<br> <b>PH : </b>" + beers[i].ph +
+                "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast +
+                "<br> <b>First Brewed : </b>" + beers[i].first_brewed +
+                "<br> <b>Description : </b>" + beers[i].description +
+                "<br>" + "<image src =" + beers[i].image_url + "/><hr>"
+        }
+        document.getElementById('result').innerHTML = result
+    })
+
+const getBeerAPIByName = (name) => fetch(`https://api.punkapi.com/v2/beers/?beer_name=${name}`)
+    .then(response => response.json())
+    .catch(e => console.err('Fail'))
+    .then(response => {
+        let beers = response;
+        let result = "";
+        for (let i = 0; i < beers.length; i++) {
+            result += "<b>Name : </b>" + beers[i].name +
+                "<br> <b>Tagline : </b>" + beers[i].tagline +
+                "<br> <b>PH : </b>" + beers[i].ph +
+                "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast +
+                "<br> <b>First Brewed : </b>" + beers[i].first_brewed +
+                "<br> <b>Description : </b>" + beers[i].description +
+                "<br>" + "<image src =" + beers[i].image_url + "/><hr>"
+        }
+        document.getElementById('result').innerHTML = result
+    })
+
+
+
+getBeersAPI()
+
+const toSearch = (name) => {
+    if (name != "") {
+         getBeerAPIByName(name)       
+    } else {
+        let result = "No result";
+        document.getElementById('result').innerHTML = result
+    }
+}
